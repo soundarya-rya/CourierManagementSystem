3342b081f5cdee97269d7c1c74d8b612f3d1f00b
soundarya-rya <107978309+soundarya-rya@users.noreply.github.com>
2023-12-19 18:09:07 +0530

    Delete CourierSys directory

--- CourierSys/src/com/hexaware/controller/ICourierAdminService.java
+++
@@ -1,6 +0,0 @@
-package com.hexaware.controller;
-
-public interface ICourierAdminService {
-	public int addCourierStaff();
-
-}
--- CourierSys/src/com/hexaware/controller/ICourierUserService.java
+++
@@ -1,8 +0,0 @@
-package com.hexaware.controller;
-
-public interface ICourierUserService {
-	public void placeOrder();
-	public void getOrderStatus();
-	public void cancelOrder();
-	public void getAssignedOrder();
-}
